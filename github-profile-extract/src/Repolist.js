53f9eaa92ed6c429e7758f1af4f5ab47ddc500b0
Anuj <anujtiwari2822@gmail.com>
2024-06-26 10:15:40 +0530

    Repository Item done

--- github-profile-extract/src/Repolist.js
+++ github-profile-extract/src/Repolist.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import RepoItem from './ReposItem';
+
+function Repolist({ repos }) {
+  return (
+    <div className="repository-list">
+      {repos.map((repo) => (
+        <RepoItem key={repo.id} repo={repo} />
+      ))}
+    </div>
+  );
+}
+
+export default Repolist;
 
